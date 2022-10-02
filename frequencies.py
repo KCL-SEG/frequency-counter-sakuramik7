8f4b54d569750ed13d25d5fdf7e7ac49b5a414a1
sakuramik7 <99272370+sakuramik7@users.noreply.github.com>
2022-10-02 16:59:36 +0100

    Algorithm for frequencies using counters

--- frequencies.py
+++ frequencies.py
@@ -3,5 +3,21 @@
 
 def frequencies(items):
     frequencies = {}
-    # Your code goes here
+    items = [str(value) for value in items]
+    lastv = len(items)
+    while 0 != lastv:
+        x = 0
+        temp = items[0]
+        items.pop(0)
+        lastv = lastv -1
+        count = 1
+        while x != lastv:
+            if (temp == items[x]):
+                count = count + 1
+                items.pop(x)
+                lastv = lastv -1
+            else:
+                x = x + 1
+
+        frequencies.update({temp:count})
     return frequencies
